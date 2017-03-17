8e38cb383b4e7e6632495341f6707ae149c81685
tekin.omer <omer-ali.tekin@teamnet.ro>
2017-03-17 11:13:26 +0200

    build reactjs prod version of bundle.js

--- src/main/react-app/Card.jsx
+++ src/main/react-app/Card.jsx
@@ -4,11 +4,10 @@ import ReactDOM from 'react-dom';
 class Card extends React.Component {
     render () {
         let card = this.props.data;
-        let imgSrc = 'resources/img/cards/' + card.code + '.png';
-        let cardClass = 'playing-card' + (card.faceDown ? ' face-down' : '');
+        let imgSrc = 'resources/img/cards/' + (card.faceDown ? 'back' : card.code) + '.png';
 
         return (
-            <div className={cardClass} >
+            <div className="playing-card" >
                 <img src={imgSrc} />
             </div>
         );
